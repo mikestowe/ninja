ec14eff9831026600ba4cf71810444d54e143e04
Ananya Sen <Ananya.Sen@motorola.com>
2012-02-14 14:53:32 -0800

    allow / in windows directory path
    Signed-off-by: Ananya Sen <Ananya.Sen@motorola.com>

--- js/io/system/coreioapi.js
+++ js/io/system/coreioapi.js
@@ -1101,7 +1101,7 @@ window.hack = function (name, type) {
                 uri = uri.replace(/^\s+|\s+$/g,"");  // strip any leading or trailing spaces
 
                 //for local machine folder uri
-                isWindowsUri = /^([a-zA-Z]:)(\\[^<>:"/\\|?*]+)*\\?$/gi.test(uri);
+                isWindowsUri = /^([a-zA-Z]:)([\\/][^<>:"/\\|?*]+)*[\\/]?$/gi.test(uri);
                 isUnixUri = /^(\/)?(\/(?![.])[^/]*)*\/?$/gi.test(uri);//folders beginning with . are hidden on Mac / Unix
                 status = isWindowsUri || isUnixUri;
                 if(isWindowsUri && isUnixUri){status = false;}
