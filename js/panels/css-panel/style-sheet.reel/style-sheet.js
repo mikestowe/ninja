72061f2cf3858f48a90f8cb21c5e7d766cb9552b
Eric Guzman <ericg@motorola.com>
2012-04-05 17:26:03 -0700

    Style Sheets - Style update for edit view

--- js/panels/css-panel/style-sheet.reel/style-sheet.js
+++ js/panels/css-panel/style-sheet.reel/style-sheet.js
@@ -8,35 +8,41 @@ var Montage = require("montage/core/core").Montage,
     Component = require("montage/ui/component").Component;
 
 exports.StyleSheet = Montage.create(Component, {
-    deserializedFromTemplate : {
-        value: function() {
-            console.log("style sheet view - deserialized");
-        }
+    _translateDistance: {
+        value: null
     },
+
     willDraw : {
         value: function() {
             console.log("style sheet view - will draw");
 
             if(this.editing) {
-                document.body.addEventListener('click', this, false);
+                document.body.addEventListener('mousedown', this, false);
+                this._translateDistance = this._element.offsetWidth - this.editButton._element.offsetWidth;
+
             } else {
-                document.body.removeEventListener('click', this, false);
+                document.body.removeEventListener('mousedown', this, false);
             }
         }
     },
     draw : {
         value: function() {
+            var transStr = '-webkit-transform';
             console.log("styles sheet view - draw");
 
             this.mediaInput.value = this._source.media.mediaText;
 
             if(this.editing) {
                 this.editView.classList.add('expanded');
+                this.editView.style.setProperty(transStr, 'translate3d(-'+ this._translateDistance + 'px,0,0)');
             } else {
                 this.editView.classList.remove('expanded');
+                this.editView.style.removeProperty(transStr);
             }
         }
     },
+
+
 
     handleEditButtonAction: {
         value: function(e) {
@@ -57,10 +63,10 @@ exports.StyleSheet = Montage.create(Component, {
         }
     },
 
-    handleClick : {
+    handleMousedown : {
         value: function(e) {
-            console.log("handle click");
-            if(e.target !== this.editView) {
+            console.log("handle mousedown");
+            if(e.target !== this.editView && e.target !== this.editButton) {
                 this.editing = false;
             }
         }
@@ -89,7 +95,7 @@ exports.StyleSheet = Montage.create(Component, {
             return this._source;
         },
         set: function(sheet) {
-            console.log('sheet being set');
+            console.log('sheet being set: ', sheet.ownerNode);
             if(sheet.href) {
                 this.name = sheet.href.substring(sheet.href.lastIndexOf('/')+1);
             } else {
