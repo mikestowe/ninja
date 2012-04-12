3bbf32b285405562471fa594c283e271d347734e
Nivesh Rajbhandari <mqg734@motorola.com>
2012-04-12 10:52:32 -0700

    Borders are not factored into offset calculations by the browser, so we have to adjust for them. Also, we should set webkit-transform-style to preserve-3d if elements are rotated (until we also support flattened mode).
    Signed-off-by: Nivesh Rajbhandari <mqg734@motorola.com>

--- js/helper-classes/3D/view-utils.js
+++ js/helper-classes/3D/view-utils.js
@@ -658,6 +658,21 @@ exports.ViewUtils = Montage.create(Component, {
         //        if (elt.__ninjaXOff)  xOff = elt.__ninjaXOff;
         //        if (elt.__ninjaYOff)  yOff = elt.__ninjaYOff;
             var offset = [xOff, yOff];
+            if(elt.offsetParent && (elt.offsetParent !== this._stageElement))
+            {
+                var pS = elt.ownerDocument.defaultView.getComputedStyle(elt.offsetParent);
+
+                var border = parseInt(pS.getPropertyValue("border"));
+
+                if(border)
+                {
+                    var bl = parseInt(pS.getPropertyValue("border-left-width")),
+                        bt = parseInt(pS.getPropertyValue("border-top-width"));
+
+                    offset[0] += bl;
+                    offset[1] += bt;
+                }
+            }
 
             if(elt === this._stageElement)
             {
