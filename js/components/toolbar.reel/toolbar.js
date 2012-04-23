4cefebd99813f05713deb2f72bba0a035dfcb508
Eric Guzman <ericg@motorola.com>
2012-04-23 11:46:54 -0700

    Toolbar - Add buttons to toolbar component

--- js/components/toolbar.reel/toolbar.js
+++ js/components/toolbar.reel/toolbar.js
@@ -8,20 +8,50 @@ var Montage = require("montage/core/core").Montage,
     Component = require("montage/ui/component").Component;
 
 exports.Toolbar = Montage.create(Component, {
-
-    deserializedFromTemplate : {
-        value: function() {
-            console.log("toolbar - deserialized");
+    _needsButtonProperties : {
+        value: null
+    },
+    _buttons : { value: null },
+    buttons : {
+        get: function() {
+            return this._buttons;
+        },
+        set: function(btns) {
+            this._buttons = btns;
+            this._needsButtonProperties = true;
+            console.log("buttons set");
         }
     },
     prepareForDraw : {
         value: function() {
             console.log("toolbar - prepare for draw");
+            if(this._needsButtonProperties) {
+                this.repetition.childComponents.forEach(function(button) {
+                    button.identifier = button.sourceObject.identifier;
+                    button.addEventListener('action', this.delegate, false);
+                }, this);
+            }
         }
     },
     draw : {
         value: function() {
-            console.log("toolbar - draw");
+            console.log("toolbar - draw - repetition ", this.repetition);
+            if(this._needsClass) {
+
+                this.repetition.childComponents.forEach(function(button) {
+                    button.element.classList.add('toolbar-' + button.sourceObject.identifier + '-button');
+                }, this);
+            }
+
+            if(this._needsButtonProperties) {
+                this._needsClass = this.needsDraw = true;
+                this._needsButtonProperties = false;
+            }
+
+
         }
+    },
+    _needsClass : {
+        value: null
     }
 });
