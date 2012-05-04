b96dd7bbf61e84be51a7e011a85d1f09f493d1e5
Valerio Virgillito <valerio@motorola.com>
2012-05-03 23:15:17 -0700

    fixing the currentContainer
    Signed-off-by: Valerio Virgillito <valerio@motorola.com>

--- js/controllers/elements/element-controller.js
+++ js/controllers/elements/element-controller.js
@@ -15,13 +15,13 @@ exports.ElementController = Montage.create(Component, {
                 var selectedLayerIndex = this.application.ninja.timeline.getLayerIndexByID(this.application.ninja.timeline.currentLayerSelected.layerData.layerID);
 
                 if(selectedLayerIndex === 0) {
-                    this.application.ninja.currentDocument.currentSelectedContainer.appendChild(el);
+                    this.application.ninja.currentSelectedContainer.appendChild(el);
                 } else {
                     var element = this.application.ninja.timeline.arrLayers[selectedLayerIndex].layerData.elementsList[0];
                     element.parentNode.insertBefore(el, element.nextSibling);
                 }
             } else {
-                this.application.ninja.currentDocument.currentSelectedContainer.appendChild(el);
+                this.application.ninja.currentSelectedContainer.appendChild(el);
             }
 
             if(styles) {
