0088f8e5f3a0be4b6e5e497afbe34de11211493c
John Mayhew <vpn847@motorola.com>
2012-05-09 11:36:13 -0700

    -Put new selection tool icons in the options bar
    -Styled the selection tool options bar

--- js/components/tools-properties/selection-properties.reel/selection-properties.js
+++ js/components/tools-properties/selection-properties.reel/selection-properties.js
@@ -14,15 +14,39 @@ exports.SelectionProperties = Montage.create(ToolProperties, {
     _controls: { value: false },
 
     _subPrepare: {
-        value: function() {
+        value: function () {
             this.transform.addEventListener("change", this, false);
+
+            // The functionality for these buttons is not currently implemented
+            // Until it is we will make them all disabled by default.
+            this.topAlign.disabled = true;
+
+            this.middleAlign.disabled = true;
+            this.bottomAlign.disabled = true;
+
+            this.leftAlign.disabled = true;
+            this.centerAlign.disabled = true;
+            this.rightAlign.disabled = true;
+
+            this.distTop.disabled = true;
+            this.distMiddle.disabled = true;
+            this.distBottom.disabled = true;
+
+            this.distLeft.disabled = true;
+            this.distCenter.disabled = true;
+            this.distRight.disabled = true;
+
+            this.arrangeBringForward.disabled = true;
+            this.arrangeSendBackward.disabled = true;
+            this.arrangeBringToFront.disabled = true;
+            this.arrangeSendToBack.disabled = true;
         }
     },
 
     handleChange: {
-        value: function(event) {
+        value: function (event) {
             this._controls = this.transform.checked;
-            NJevent("toolOptionsChange", {source: "SelectionProperties", inTransformMode: this.transform.checked});
+            NJevent("toolOptionsChange", { source: "SelectionProperties", inTransformMode: this.transform.checked });
         }
     }
 
