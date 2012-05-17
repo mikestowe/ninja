e8ae5db7ce7023b638375cbc27a3f7b7a2f77b23
Armen Kesablyan <armen@motorola.com>
2012-05-17 13:59:23 -0700

    Removed Old Objects Panel Structure
    Signed-off-by: Armen Kesablyan <armen@motorola.com>

--- js/panels/objects.reel/objects.js
+++
@@ -1,7 +0,0 @@
-var Montage =	require("montage/core/core").Montage,
-    PanelBase =	require("js/panels/panelbase").PanelBase;
-////////////////////////////////////////////////////////////////////////
-//Exporting as ColorPanel
-exports.ObjectsPanel = Montage.create(PanelBase, {
-
-});
