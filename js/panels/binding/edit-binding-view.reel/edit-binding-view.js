d7e39dd2ff310b9f05676b7de49756036ab03514
Eric Guzman <ericg@motorola.com>
2012-06-08 15:57:08 -0700

    Binding Panel - Update edit view - populate binding arguments

--- js/panels/binding/edit-binding-view.reel/edit-binding-view.js
+++ js/panels/binding/edit-binding-view.reel/edit-binding-view.js
@@ -9,8 +9,77 @@ var Montage = require("montage/core/core").Montage,
 
 
 exports.EditBindingView = Montage.create(Component, {
-    objectsList : {
-        value: ["Object1", "Object2", "Object3"]
+
+    /* -------------------
+       Binding Properties
+     ------------------- */
+
+    sourceObjectIdentifier : {
+        value: "",
+        distinct: true
+    },
+    sourceObjectPropertyPath : {
+        value: "",
+        distinct: true
+    },
+    boundObjectIdentifier : {
+        value: "",
+        distinct: true
+    },
+    boundObjectPropertyPath : {
+        value: "",
+        distinct: true
+    },
+    _oneway: {
+        value: null
+    },
+    oneway: {
+        get: function() {
+            return this._oneway;
+        },
+        set: function(value) {
+            if(value === this._oneway) { return; }
+
+            this._oneway = !!value;
+
+            this.needsDraw = true;
+        }
+    },
+
+    /* -------------------
+     Binding Args Object
+     ------------------- */
+
+    _bindingArgs : {
+        value: null
+    },
+    bindingArgs :{
+        get: function() {
+            return this._bindingArgs;
+        },
+        set: function(value) {
+            if(value === this._bindingArgs) { return; }
+
+            this._bindingArgs = value;
+
+            this.sourceObjectIdentifier = value.sourceObject.identifier;
+            this.sourceObjectPropertyPath = value.sourceObjectPropertyPath;
+            this.boundObjectIdentifier = value.boundObject.identifier;
+            this.boundObjectPropertyPath = value.boundObjectPropertyPath;
+            this.oneway = value.oneway;
+
+            this.needsDraw = true;
+        }
+    },
+
+    /* -------------------
+     Save/Close button handlers
+     ------------------- */
+
+    handleCloseButtonAction : {
+        value: function(e) {
+            this.parentComponent.editing = false;
+        }
     },
 
     prepareForDraw : {
