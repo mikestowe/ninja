f8643c11746786f8e47801bbc510b246d337a71b
Jonathan Duran <jduran@motorola.com>
2012-06-26 10:40:01 -0700

    Fix end animation for style tracks
    Signed-off-by: Jonathan Duran <jduran@motorola.com>

--- js/panels/Timeline/PropertyTrack.reel/PropertyTrack.js
+++ js/panels/Timeline/PropertyTrack.reel/PropertyTrack.js
@@ -479,8 +479,8 @@ var PropertyTrack = exports.PropertyTrack = Montage.create(Component, {
             this.parentComponent.parentComponent.parentComponent.animationNamesString = newAnimationNames;
 
             this.ninjaStylesContoller.setElementStyle(this.animatedElement, "-webkit-animation-name", newAnimationNames);
-
             this.ninjaStylesContoller.setElementStyle(this.animatedElement, "-webkit-animation-duration", newAnimationDuration);
+            this.ninjaStylesContoller.setElementStyle(this.animatedElement, "-webkit-animation-fill-mode", "forwards");
             this.ninjaStylesContoller.setElementStyle(this.animatedElement, "-webkit-animation-iteration-count", newIterationCount);
 
             var initRule = "@-webkit-keyframes " + this.animationName + " { 0% {" + this.trackEditorProperty + ": " + currentStyleValue + ";} 100% {" + this.trackEditorProperty + ": " + currentStyleValue + ";} }";
